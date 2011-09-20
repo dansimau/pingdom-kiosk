732ab2251f28aea1767c83818c90e373832a3f45
Daniel Simmons <dan@dans.im>
2011-09-20 17:59:40 +0100

    Refactor config file loading
    Fix parseJsonFromFile() to make it truly asynchronous; add
    parseJsonFromFileSync() method (which we'll use here) for reading the
    config synchronously.

--- monitor-console.js
+++ monitor-console.js
@@ -3,37 +3,34 @@ var pingdom = require("./lib/pingdom.js");
 var config = require("./lib/config.js");
 
 // Read config from file
-config.parseJsonFromFile(__dirname + '/kiosk-server.conf', function(conf, err) {
+var conf = config.parseJsonFromFileSync(__dirname + '/kiosk-server.conf');
+if (!conf) {
+	console.log("Failed parsing configuration file. Exiting.");
+	process.exit(10);
+}
 
-	if (err) {
-		console.log("Failed parsing configuration file. Exiting.");
-		process.exit(10);
-	}
+// Determine API server config
+var api = conf.pingdom.apiserver || {
+	"host": "",
+	"port": "",
+	"protocol": "",
+	"pollfreq": ""
+};
 
-	// Determine API server config
-	var api = conf.pingdom.apiserver || {
-		"host": "",
-		"port": "",
-		"protocol": "",
-		"pollfreq": ""
-	};
+// Create pingdom API connectors
+var connectors = [];
+for (var i = 0, l = conf.pingdom.accounts.length; i < l; i++) {
+	conf.pingdom.accounts[i]["host"] = api.host;
+	conf.pingdom.accounts[i]["port"] = api.port;
+	conf.pingdom.accounts[i]["protocol"] = api.protocol;
+	conf.pingdom.accounts[i]["pollfreq"] = api.pollfreq;
+	connectors.push(pingdom.createConnector(conf.pingdom.accounts[i]));
+}
 
-	// Create pingdom API connectors
-	var connectors = [];
-	for (var i = 0, l = conf.pingdom.accounts.length; i < l; i++) {
-		conf.pingdom.accounts[i]["host"] = api.host;
-		conf.pingdom.accounts[i]["port"] = api.port;
-		conf.pingdom.accounts[i]["protocol"] = api.protocol;
-		conf.pingdom.accounts[i]["pollfreq"] = api.pollfreq;
-		connectors.push(pingdom.createConnector(conf.pingdom.accounts[i]));
-	}
+// Create pingdom middleware monitor
+var monitor = pingdom.createMonitor(connectors);
 
-	// Create pingdom middleware monitor
-	var monitor = pingdom.createMonitor(connectors);
-
-	// Subscribe to the status change event
-	monitor.addListener('statusChange', function(data) {
-		console.log("OMG! Something happened!");
-	});
-
+// Subscribe to the status change event
+monitor.addListener('statusChange', function(data) {
+	console.log("OMG! Something happened!");
 });
